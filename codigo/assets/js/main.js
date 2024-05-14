3c8db076a1fbf3b3f55517fc4c297a929aab160c
davif <davifpuddo@gmail.com>
2024-05-14 17:36:56 -0300

    Conversor de texto para MK

---
+++ codigo/assets/js/main.js
@@ -0,0 +1,18 @@
+
+    const text = document.querySelector("#markdownText");
+    const preview = document.querySelector("#preview");
+    const upload = document.querySelector("#upload");
+
+    text.addEventListener("input", function(){   
+
+        // Conversao para Markdown
+        const markdown = marked.parse(text.value);
+
+        // Mostrar resultado
+        preview.innerHTML = DOMPurify.sanitize(markdown,
+            {USE_PROFILES: {html: true}});
+    });
+
+    upload.addEventListener("click", function(){
+        text.append.innerHTML = 123;
+    })
