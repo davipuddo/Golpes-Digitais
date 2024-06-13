ee8eb090ffb1928c76ac2ab195c0aa8ec840eafc
davif <davifpuddo@gmail.com>
2024-06-13 20:14:59 -0300

    Barra de pesquisa (Matheus), pagina da noticia, responsividade da home

---
+++ codigo/assets/js/document.js
@@ -0,0 +1,23 @@
+const logo = document.querySelector("#Logomarca");
+const text = document.querySelector("#doc");
+
+// Ir para homepage
+logo.addEventListener("click", function(){
+    window.location.href = "index.html";
+})
+
+var data = {};
+if (localStorage.getItem('db'))
+{
+    let n = parseInt(window.location.search[3]);
+    data = JSON.parse(localStorage.getItem('db'))[n].text;
+    text.innerHTML = marked.parse(data);
+}
+
+if (window.matchMedia("(max-width:650px)").matches)
+{
+    if (!confirm("Resolução não suportada deseja continuar?"))
+    {
+        window.location.href = "index.html";
+    }
+}
--- codigo/assets/js/main.js
+++ codigo/assets/js/main.js
@@ -5,7 +5,10 @@ const CardArea = document.querySelector("main div");
 const Editor = document.querySelector("#Edit");
 const About = document.querySelector("#About");
 const Delete = document.querySelector("#Delete");
+const search = document.querySelector("#Search");
+
 var NQ = 0;
+var CardsReady = false;
 
 // Ir para homepage
 logo.addEventListener("click", function(){
@@ -25,16 +28,17 @@ About.addEventListener("click", function(){
 // Criar Cards baseados no DB
 function Cards()
 {
+    CardsReady = true;
     var data = JSON.parse(localStorage.getItem('db'));
     var TN = data.length
     for (let i = 0; i < TN; i++)
     {
-        CardArea.innerHTML += `<div class="card" style="width: 28rem; height: 18rem">
+        CardArea.innerHTML += `<div class="card">
                                 <div class="text">
                                    <h3 class="card-title">${data[i].title}</h3>
                                    <p class="card-subtitle">${data[i].subtitle}</p>
                                 </div>
-                                   <button id="${i}">Visitar</button>
+                                   <button id="${i}" class="GoToDoc">Visitar</button>
                                </div>`
     }
 }
@@ -46,9 +50,46 @@ Delete.addEventListener("click", function(){
 })
 
 // Carregar cards
-document.addEventListener("DOMContentLoaded", function(){
-    if (localStorage.getItem('db'))
-        {
-            Cards();
-        }
-})
+
+ if (localStorage.getItem('db'))
+{
+    Cards();  
+}
+
+if (CardsReady)
+{
+    // Selecionar noticia
+    const Docs = document.querySelectorAll('.GoToDoc');
+    for (let i = 0; i < Docs.length; i++)
+    {
+            Docs[i].addEventListener("click", function(){
+                let link = window.location.origin + '/codigo/document.html';
+                let url = new URL(link);
+                url.searchParams.append('n', i);
+                let modifiedUrl = url.toString();
+                window.location.href = modifiedUrl
+                delete(url);
+            })
+    }
+
+    // Pesquisar
+    document.addEventListener('DOMContentLoaded', function () {
+        const searchInput = document.getElementById('searchInput');
+    
+        searchInput.addEventListener('input', function () {
+          const searchTerm = this.value.toLowerCase();
+    
+          const animalCards = document.querySelectorAll('.card');
+    
+          animalCards.forEach(function (card) {
+            const titulo = card.querySelector('h3:first-of-type').textContent.toLowerCase();
+    
+            if (titulo.includes(searchTerm)) {
+              card.style.display = 'block';
+            } else {
+              card.style.display = 'none';
+            }
+          });
+        });
+    });
+}
