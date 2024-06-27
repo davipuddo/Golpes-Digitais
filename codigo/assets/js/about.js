e899e85e42e75ed53c2999051c116d909485ffac
davipuddo <davifpuddo@gmail.com>
2024-06-27 16:27:51 -0300

    FAQ

--- codigo/assets/js/about.js
+++ codigo/assets/js/about.js
@@ -158,3 +158,17 @@ var interval = setInterval(function(){
   };
   money.innerHTML = x + " Bilhões de reais";
 }, 40);
+
+const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(faqItem => {
+      const faqQuestion = faqItem.querySelector('.faq-question');
+      const faqAnswer = faqItem.querySelector('.faq-answer');
+      const faqToggle = faqQuestion.querySelector('.faq-toggle');
+
+      faqQuestion.addEventListener('click', () => {
+        faqItem.classList.toggle('active');
+        faqToggle.textContent = faqItem.classList.contains('active') ? '-' : '+';
+    });
+    });
+    
